8e88ea7dd9261ae6562ebc60c5a3610c77847cb2
tharunk4743 <tharunk4743@gmail.com>
2025-12-26 21:18:59 +0530

    Update schemas.py

--- server/schemas.py
+++ server/schemas.py
@@ -1,21 +1,12 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
 class SignupRequest(BaseModel):
     email: str
-    full_name: Optional[str]
+    full_name: Optional[str] = None
     password: str
     role: Optional[str] = "EMPLOYEE"
-
 
 class LoginRequest(BaseModel):
     email: str
     password: str
-    role: Optional[str] = "EMPLOYEE"
-
-
-class NotificationPayload(BaseModel):
-    user_id: str
-    title: str
-    content: str
